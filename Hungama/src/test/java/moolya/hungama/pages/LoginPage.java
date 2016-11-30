40d0ef741c8dd30f19a47485f73ced371d896e48
Gagan KV <gagan.kv@moolya.com>
2016-11-30 14:44:20 +0530

    Hungama :: Added LoginPage

---
+++ Hungama/src/test/java/moolya/hungama/pages/LoginPage.java
@@ -0,0 +1,17 @@
+package moolya.hungama.pages;
+
+import org.openqa.selenium.support.PageFactory;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+
+public class LoginPage extends M_BasePage {
+
+	public LoginPage(AppiumDriver<MobileElement> mdriver) {
+		super(mdriver);
+		PageFactory.initElements(new AppiumFieldDecorator(mdriver), this);
+		// TODO Auto-generated constructor stub
+	}
+
+}
