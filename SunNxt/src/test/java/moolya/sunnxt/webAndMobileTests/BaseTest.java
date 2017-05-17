6747a3324bbd11a26c34ac198375e99592c96028
Yatheendra BC <yatheendra@moolya.com>
2017-05-17 14:28:17 +0530

    Added SunNxt Automation project

---
+++ SunNxt/src/test/java/moolya/sunnxt/webAndMobileTests/BaseTest.java
@@ -0,0 +1,95 @@
+package moolya.sunnxt.webAndMobileTests;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+
+import atu.testrecorder.ATUTestRecorder;
+import atu.testrecorder.exceptions.ATUTestRecorderException;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import moolya.sunnxt.pages.mobilepages.M_BasePage;
+import moolya.sunnxt.pages.webpages.W_BasePage;
+import moolya.sunnxt.utils.AppiumServerUtils;
+
+public class BaseTest {
+
+
+	public AppiumDriver<MobileElement> mdriver;
+	public WebDriver wdriver;
+
+	W_BasePage wbasepage;
+
+	M_BasePage mbasepage;
+	ATUTestRecorder recorder;
+
+	public void launchMobileApp() throws IOException, InterruptedException{
+		AppiumServerUtils.startAppium();
+		mbasepage = new M_BasePage(mdriver);
+		mdriver = mbasepage.launchmobApp();
+	}
+
+	public void launchWebApp() throws IOException, ATUTestRecorderException{
+		String dir = System.getProperty("user.dir");
+		DateFormat dateFormat = new SimpleDateFormat("yy-MM-dd HH-mm-ss");
+		Date date = new Date();
+		recorder = new ATUTestRecorder(dir+"\\script-videos\\", "Verify-Order-Summary-" + dateFormat.format(date), false);
+		// To start video recording.
+		wbasepage = new W_BasePage(wdriver);
+		recorder.start();
+		wdriver = wbasepage.launchWebApp();
+	}
+
+	public void closeMobileApp() throws IOException, InterruptedException
+	{
+		mdriver.closeApp();
+		AppiumServerUtils.stopAppium();
+	}
+	
+	public void closeWebApp() throws ATUTestRecorderException{
+		wdriver.close();
+		recorder.stop();
+	}
+
+
+	public void catchExceptions(ITestResult result,WebDriver driver){
+		String dir = System.getProperty("user.dir");
+		String[] clsParts = result.getInstanceName().split("\\.");
+		String clsName = clsParts[(clsParts.length)-1];
+		if(!result.isSuccess()){            
+			File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			String file =dir+"/screenshots/"+clsName+".png";
+			try {
+				FileUtils.copyFile(scrFile, new File(file));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	public void catchExceptions(ITestResult result,AppiumDriver driver) throws IOException, InterruptedException 
+	{    
+		String dir = System.getProperty("user.dir");
+		String[] clsParts = result.getInstanceName().split("\\.");
+		String clsName = clsParts[(clsParts.length)-1];
+		if(!result.isSuccess()){            
+			File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			String file =dir+"/screenshots/"+clsName+".png";
+			try {
+				FileUtils.copyFile(scrFile, new File(file));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
---
+++ SunNxt/src/test/java/moolya/sunnxt/webTests/W_BaseTest.java
@@ -0,0 +1,67 @@
+package moolya.sunnxt.webTests;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+
+import atu.testrecorder.ATUTestRecorder;
+import atu.testrecorder.exceptions.ATUTestRecorderException;
+import moolya.sunnxt.pages.webpages.W_BasePage;
+
+public class W_BaseTest 
+{
+
+public WebDriver wdriver;
+	
+	W_BasePage basepage;
+
+	ATUTestRecorder recorder;
+	
+	@BeforeClass
+	public void setUp() throws IOException, ATUTestRecorderException{
+		String dir = System.getProperty("user.dir");
+		DateFormat dateFormat = new SimpleDateFormat("yy-MM-dd HH-mm-ss");
+		Date date = new Date();
+		recorder = new ATUTestRecorder(dir+"\\script-videos\\", "Verify-Order-Summary-" + dateFormat.format(date), false);
+		// To start video recording.
+		basepage = new W_BasePage(wdriver);
+		recorder.start();
+		wdriver = basepage.launchWebApp();
+	}
+	
+	@AfterMethod
+	public void catchExceptions(ITestResult result) throws IOException, InterruptedException 
+	{    
+		String dir = System.getProperty("user.dir");
+		String[] clsParts = result.getInstanceName().split("\\.");
+		String clsName = clsParts[(clsParts.length)-1];
+		if(!result.isSuccess()){            
+			File scrFile = ((TakesScreenshot)wdriver).getScreenshotAs(OutputType.FILE);
+			String file =dir+"/screenshots/"+clsName+".png";
+			try {
+				FileUtils.copyFile(scrFile, new File(file));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@AfterClass
+	public void tearDown() throws ATUTestRecorderException{
+		wdriver.close();
+		recorder.stop();
+	}
+	
+}
